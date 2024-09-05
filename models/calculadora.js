e19830ac230172157758e168ec273465c5116111
Taleko <47725439+tales96323@users.noreply.github.com>
2024-09-05 10:30:50 +0000

    Inicio da fase de testes

---
+++ models/calculadora.js
@@ -0,0 +1,21 @@
+function somar(num1, num2) {
+  return num1 + num2;
+}
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
+function subtrair(num1, num2) {
+  return num1 - num2;
+}
+function dividir(num1, num2) {
+  return num1 / num2;
+}
+function raiz(num1) {
+  return Math.sqrt(num1);
+}
+
+exports.somar = somar;
+exports.multiplicar = multiplicar;
+exports.subtrair = subtrair;
+exports.dividir = dividir;
+exports.raiz = raiz;
