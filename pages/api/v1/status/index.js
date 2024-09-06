f68b80516d54d09a8affca976fea2146e37946c7
Taleko <47725439+tales96323@users.noreply.github.com>
2024-09-06 15:10:03 +0000

    add `database.js` and tret

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
+import database from "../../../../infra/database.js";
+
 // cria uma */api/status - mas precisa de uma função
-//
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Tá tudo certo!" });
 }
 
