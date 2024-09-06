71e3399ad1b2f4dd2915c010a2cd94362a8a303e
Taleko <47725439+tales96323@users.noreply.github.com>
2024-09-06 15:44:38 +0000

    add services scripts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3e37c361fdaad56d"; }
+/******/ 		__webpack_require__.h = function() { return "12e12d863c78e991"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
