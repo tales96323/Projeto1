875de67de065ce3c3ea6d67b59d44f24ed096082
Taleko <47725439+tales96323@users.noreply.github.com>
2024-09-08 18:00:07 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,35 @@ import database from "infra/database.js";
 // cria uma */api/status - mas precisa de uma função
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+
+  //esse comando puxa o que tá na string, leva pro banco, processa e devolve pra cá. Isso é uma consulta ao banco. Pode ser SELECT, UPDATE, DELETE, SHOW...
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
   });
 }
 
--- test/integrations/api/v1/status/get.test.js
+++ test/integrations/api/v1/status/get.test.js
@@ -5,9 +5,16 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  console.log(responseBody);
   expect(responseBody.updated_at).toBeDefined();
 
   new Date(responseBody.updated_at).toISOString();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependecies.database.version).toEqual("16.4");
+
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
 });
